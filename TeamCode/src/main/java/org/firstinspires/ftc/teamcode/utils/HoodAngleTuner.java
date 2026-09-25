de0f2adaddba7e5ea9fadf8cd9fa34c1ddc1f049
CatChaos8 <chaospatato@gmail.com>
2026-09-24 20:24:23 -0400

    Added the hood servo tuner thingy

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/HoodAngleTuner.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class HoodAngleTuner extends OpMode {
+
+    private double angle = 45;
+    private double delta = 22.5;
+
+    private Servo hoodServo;
+
+    @Override
+    public void init() {
+        hoodServo = hardwareMap.get(Servo.class, "hood");
+    }
+
+    @Override
+    public void loop() {
+        hoodServo.setPosition(angle / 180.0*Math.PI);
+        if(gamepad1.dpadUpWasPressed()) {
+            angle += delta;
+            delta /= 2;
+        }
+        if(gamepad1.dpadDownWasPressed()) {
+            angle -= delta;
+            delta /= 2;
+        }
+    }
+}
